1f3ac241b6b26bd2d137496ee55a20415f627f8c
pacocoursey <pvco.coursey@gmail.com>
2018-11-05 10:56:10 -0700

    fix links

--- link.sh
+++ link.sh
@@ -1,11 +1,11 @@
 # Link bash profile
 rm ~/.bash_profile
-ln -s ./.bash_profile ~/.bash_profile
+ln -s ~/dev/dotfiles/.bash_profile ~/.bash_profile
 
 # Link hyper config
 rm ~/.hyper.js
-ln -s ./.hyper.js ~/.hyper.js
+ln -s ~/dev/dotfiles/.hyper.js ~/.hyper.js
 
 # Link atom snippets
 rm ~/.atom/snippets.cson
-ln -s ./snippets.cson ~/.atom/snippets.cson
+ln -s ~/dev/dotfiles/snippets.cson ~/.atom/snippets.cson

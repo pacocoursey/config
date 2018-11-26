0728c6132d21af9e4d126808f69aad11e525ea40
pacocoursey <pvco.coursey@gmail.com>
2018-11-26 14:40:14 -0700

    add stylelint and eslint configs

--- link.sh
+++ link.sh
@@ -9,3 +9,11 @@ ln -s ~/dev/dotfiles/.hyper.js ~/.hyper.js
 # Link atom snippets
 rm ~/.atom/snippets.cson
 ln -s ~/dev/dotfiles/snippets.cson ~/.atom/snippets.cson
+
+# Link eslint config
+rm ~/.eslintrc
+ln -s ~/dev/dotfiles/.eslintrc ~/.eslintrc
+
+# Link stylelint config
+rm ~/.stylelintrc
+ln -s ~/dev/dotfiles/.stylelintrc ~/.stylelintrc
